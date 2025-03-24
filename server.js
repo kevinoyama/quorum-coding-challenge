c3050982dc56a454aace4b532e2e5505604dff78
kevinoyama <kevinoyama01@gmail.com>
2025-03-24 11:35:07 -0300

    endpoint list all legislators and the count of votes

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ const { parseFile } = require('./parser/csv-parser');
 const app = express();
 const port = process.env.PORT;
 
-const bills = parseFile('./data/bills_(2).csv');
-const legislators = parseFile('./data/legislators_(2).csv');
-const votes = parseFile('./data/votes_(2).csv');
-const voteResults = parseFile('./data/vote_results_(2).csv');
+app.get('/legislators/votes', async (req, res) => {
+    
+    const legislators = await parseFile('./data/legislators_(2).csv')
+    const voteResults = await parseFile('./data/vote_results_(2).csv')
+    const response = [];
 
+    for (let i = 0; i < legislators.length; i++) {
 
-app.get('/', (req, res) => {
-    res.send('this is a test');
+        const legislator = legislators[i];
+
+        const votes = voteResults.filter(vote => vote.legislator_id == legislator.id);
+
+        response.push({
+            id: legislator.id,
+            name: legislator.name,
+            supportedBillsCount: votes.reduce((acc, curr) => curr.vote_type == '1' ? acc + 1 : acc, 0),
+            opposedBillsCount: votes.reduce((acc, curr) => curr.vote_type == '2' ? acc + 1 : acc, 0)
+        });
+    }
+
+    return res.send(response);
+
 });
 
 app.listen(port, () => {

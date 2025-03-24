5db9df967d1b8392467c064f1a365fa564f09b0d
kevinoyama <kevinoyama01@gmail.com>
2025-03-24 11:27:14 -0300

    Make csv-parser async

--- parser/csv-parser.js
+++ parser/csv-parser.js
@@ -2,20 +2,21 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 const parseFile = (filePath) => {
-    const data = [];
-    fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-            data.push(row);
-        })
-        .on('close', () => {
-            console.log(`File ${filePath} loaded`);
-        })
-        .on('error', () => {
-            throw new Error(`Unable to parse CSV file ${filePath}`);
-        });
-
-    return data;
+    return new Promise((resolve, reject) => {
+        let data = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (row) => {
+               data.push(row)
+            })
+            .on('end', () => {
+                console.log(`File ${filePath} loaded`);
+                resolve(data);
+            })
+            .on('error', () => {
+                reject(`Unable to parse CSV file ${filePath}`);
+            });
+    });
 }
 
 module.exports = {
